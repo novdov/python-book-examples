a568f5f870f5518fd721325e94df566d5e13d773
novdov <rpwjd56@gmail.com>
2020-01-19 21:58:44 +0900

    ccip:ch06) Add docstring

--- clean-code-in-python/ch06/descriptors_01.py
+++ clean-code-in-python/ch06/descriptors_01.py
@@ -6,6 +6,19 @@ logger = logging.getLogger(__name__)
 
 class DescriptorClass:
     def __get__(self, instance, owner):
+        """
+        Reference: https://docs.python.org/3/reference/datamodel.html#object.__get__
+
+        Called to get the attribute of the owner class (class attribute access)
+        or of an instance of that class (instance attribute access).
+        The optional owner argument is the owner class,
+        while instance is the instance that the attribute was accessed through,
+        or None when the attribute is accessed through the owner.
+
+        Args:
+            instance: Object that calls descriptor. (client object)
+            owner: Class of this object. (ClientClass)
+        """
         if instance is None:
             return self
         logger.info(f"Call: {self.__class__.__name__}.__get__({repr(instance)}, {repr(owner)})")
