535e3165dc05b055f5586bcf2b8f05d1e031c0a4
novdov <rpwjd56@gmail.com>
2020-02-23 11:30:33 +0900

    py_django:ch02) Implement models

--- py_django/ch02/bookmark/admin.py
+++ py_django/ch02/bookmark/admin.py
@@ -1,3 +1,7 @@
-# from django.contrib import admin
+from bookmark.models import Bookmark
+from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "url")
