74fb23d12ff89a4c5c27a15ffc3dced74e5a1f44
novdov <rpwjd56@gmail.com>
2020-02-23 12:46:15 +0900

    Update urls.py

--- py_django/ch02/mysite/urls.py
+++ py_django/ch02/mysite/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from bookmark.views import BookmarkDV, BookmarkLV
 from django.contrib import admin
 from django.urls import path
+
+from bookmark.views import BookmarkDV, BookmarkLV  # isort:skip
 
 urlpatterns = [
     path("admin/", admin.site.urls),
